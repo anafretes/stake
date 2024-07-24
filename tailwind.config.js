872b8bb7737c49d7d66e66d7fddfefd2b200c1cb
Ana Fretes <anafretes@gmail.com>
2024-07-24 17:48:26 -0400

    Add daisyui theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,5 +10,8 @@ module.exports = {
     require("@tailwindcss/typography"),
     require("@tailwindcss/container-queries"),
     require("daisyui")
-  ]
+  ],
+  daisyui: {
+    themes: ["light"],
+  }
 }
